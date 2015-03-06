9388b990b324851170599578f4ecb1338c03bd88
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-06 16:31:32 -0600

    no more symlinks

--- logic.js
+++ logic.js
@@ -1,1 +1,41 @@
-/home/riley/sites/testingjs/logic.js
+var elems = []
+var elem = 0;
+elems.push($("#base"))
+function insertThing() {
+	var text_area = $("#words")
+	var toinsert = (text_area.val() != "") ? text_area.val() : "none"
+	elem++
+	var inhtml = "<div><p>" + toinsert + "</p><button type=\"button\" onclick=\"removeThing(" + elem + ")\">done</button></div>"
+	console.log(inhtml);
+	var newelem = $(inhtml)
+	elems[elems.length - 1].after($(newelem))
+	elems[elem] = newelem
+	$(text_area).val("")
+	$(text_area).focus()
+	return newelem
+}
+function removeThing(index) {
+	if (index > 0) 
+		$(elems.splice(index, 1).pop().remove())
+}
+
+function render() {
+	var laste = elems[0]
+	for (var e in elems) {
+		if e != laste
+			e.insertAfter(laste)
+		laste = e
+	}
+}
+
+$(document).keydown(function (e) {
+	console.log("key " + e.which + " pressed\n")
+	switch (e.which) {
+		case 13:
+		insertThing()
+		e.preventDefault()
+		break
+		default:
+		return
+	}
+})
