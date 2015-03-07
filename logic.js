9790c9b654f69f4dd2c572f26a7afe4d93e452cd
Riley Stewart <riley.a.stewart@vanderbilt.edu>
2015-03-06 21:46:57 -0600

    Added separate css

--- logic.js
+++ logic.js
@@ -1,31 +1,28 @@
-var elems = []
-var elem = 0;
-elems.push($("#base"))
+var inserts = []
+var w1 = $("#w1")
+
 function insertThing() {
 	var text_area = $("#words")
 	var toinsert = (text_area.val() != "") ? text_area.val() : "none"
-	elem++
-	var inhtml = "<div><p>" + toinsert + "</p><button type=\"button\" onclick=\"removeThing(" + elem + ")\">done</button></div>"
-	console.log(inhtml);
-	var newelem = $(inhtml)
-	elems[elems.length - 1].after($(newelem))
-	elems[elem] = newelem
+	
+	inserts.push(toinsert)
 	$(text_area).val("")
 	$(text_area).focus()
-	return newelem
-}
-function removeThing(index) {
-	if (index > 0) 
-		$(elems.splice(index, 1).pop().remove())
+	makeElems()
+	return 
 }
 
-function render() {
-	var laste = elems[0]
-	for (var e in elems) {
-		if e != laste
-			e.insertAfter(laste)
-		laste = e
+function makeElems() {
+	var base = []
+	for (var i = 0; i < inserts.length; i++) {
+		base.push("<div class='elemcontainer'><p class='elem'>" + inserts[i] + "</p><button class='elem' type=\"button\" onclick=\"removeThing(" + i + ")\">done</button></div>")
 	}
+	$(w1).html(base.join("\n"))
+}
+
+function removeThing(index) {	
+	inserts.splice(index, 1)
+	makeElems()
 }
 
 $(document).keydown(function (e) {
@@ -33,6 +30,11 @@ $(document).keydown(function (e) {
 	switch (e.which) {
 		case 13:
 		insertThing()
+		e.preventDefault()
+		break
+		case 27:
+		if (inserts.length > 0)
+			removeThing(inserts.length - 1)
 		e.preventDefault()
 		break
 		default:
